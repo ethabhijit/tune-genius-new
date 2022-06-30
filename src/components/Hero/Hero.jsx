06f7e6bca5382dab9105aa9edd950e5292b9cc35
ethabhijit <coder.abhijit90@gmail.com>
2022-06-30 15:40:26 +0530

    update: design

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,29 +1,18 @@
 import React from "react";
 
-import HeroImage from "../../assets/images/hero.png";
-
 import "./Hero.css";
 
 const Hero = () => {
   return (
-    <div className="container col-xxl-8 px-4 py-5">
+    <div className="hero__container">
+      <div className="container col-xxl-8 px-4 py-5 ">
       <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-        <div className="col-10 col-sm-8 col-lg-6">
-          <img
-            src={HeroImage}
-            className="d-block mx-lg-auto img-fluid"
-            alt="Bootstrap Themes"
-            width="700"
-            height="500"
-            loading="lazy"
-          />
-        </div>
-        <div className="col-lg-6">
-          <h1 className="display-6 lh-1 mb-3 hero__header">
+        <div className="mx-auto">
+          <h1 className="display-6 lh-1 mb-3 hero__header text-center">
             The stock market for music
           </h1>
-          <p className="lead hero__sub__header">Earn passive income from the music you love.</p>
-          <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+          <p className="lead hero__sub__header text-center">Earn passive income from the music you love.</p>
+          <div className="d-grid gap-2 d-md-flex justify-content-md-center">
             <button
               type="button"
               className="custom__btn px-4 me-md-2"
@@ -39,6 +28,7 @@ const Hero = () => {
           </div>
         </div>
       </div>
+    </div>
     </div>
   );
 };
